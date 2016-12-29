b490e74e60813ebe8bfd7037379c2da7be3113f1
Jack Howerton <jackhowerton@gmail.com>
2016-12-29 13:21:42 -0800

    6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(num)
+  	num.times {
+  		puts "woof"
+  	}
+  end
+
+  def rollover
+  	puts "*rolls over*"
+  end
+
+  def dog_years(input)
+  	x = input * 7
+  	puts "dog years: #{x}"
+  end
+  
+  def playdead
+    puts "*plays dead*"
+  end
+
 end
 
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(3)
+fido.dog_years(8)
+fido.rollover
+fido.playdead
