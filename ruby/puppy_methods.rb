947f9d474d905336f00ac73d59fa2dea2c5ecaa1
Jack Howerton <jackhowerton@gmail.com>
2016-12-29 13:37:12 -0800

    6.2 finished

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -24,11 +24,47 @@ class Puppy
     puts "*plays dead*"
   end
 
+  def initialize
+  	puts "initializing new puppy instance..."
+  end
+
 end
 
-fido = Puppy.new
-fido.fetch("ball")
-fido.speak(3)
-fido.dog_years(8)
-fido.rollover
-fido.playdead
+# fido = Puppy.new
+# fido.fetch("ball")
+# fido.speak(3)
+# fido.dog_years(8)
+# fido.rollover
+# fido.playdead
+
+
+class Gingerbreadman
+
+	def initialize
+		puts "initializing...beware the ginger bread man!"
+	end
+
+	def run(x)
+		x.times {
+			puts "You can't catch me, I'm the ginger bread man!"
+		}
+	end
+
+	def favoritecookie
+		puts "I prefer snickerdoodles."
+	end
+
+end
+
+gingerbreadmen = []
+
+50.times {
+	gingerbreadmen << Gingerbreadman.new
+}
+
+gingerbreadmen.each do |gingerbreadman|
+  gingerbreadman.run(3)
+  gingerbreadman.favoritecookie
+end
+
+
