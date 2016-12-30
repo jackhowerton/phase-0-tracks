64f5444f1251c4e201c5adaefbe9800bd31ae0fb
Jack Howerton <jackhowerton@gmail.com>
2016-12-30 10:24:12 -0800

    6.4 release 2

---
+++ ruby/flight.rb
@@ -0,0 +1,20 @@
+module Flight
+	def take_off(altitude)
+		puts "Taking off and ascending until reaching #{altitude} ..."
+	end
+end
+
+class Bird
+	include Flight
+end
+
+class Plane
+	include Flight
+end
+
+
+jeffthebird = Bird.new
+jeffthebird.take_off(999)
+
+alextheplane = Plane.new
+alextheplane.take_off(30000)
--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+  	words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+  	words + "!!!" + " :D"
+  end
+
 end
+
+Shout.yelling_happily("muwahahaha")
+Shout.yell_angrily("no no no no")
