3aefeb3f522f26c42859f1a25e0cec91f0d72104
Jack Howerton <jackhowerton@gmail.com>
2016-12-30 10:37:37 -0800

    6.4 release 3 complete

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,34 @@
+# module Shout
+#   def self.yell_angrily(words)
+#   	words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#   	words + "!!!" + " :D"
+#   end
+
+# end
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
   	words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
   	words + "!!!" + " :D"
   end
-
 end
 
-Shout.yelling_happily("muwahahaha")
-Shout.yell_angrily("no no no no")
+class Penguin
+	include Shout
+end
+
+class Saltshaker
+	include Shout
+end
+
+raul = Penguin.new
+raul.yelling_happily("muwahahaha")
+
+zack = Saltshaker.new
+zack.yell_angrily("no no no no")
