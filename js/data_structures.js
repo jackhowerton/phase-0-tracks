7581d9c23184b134d4e4f8755586b28a747e12f3
Jack Howerton <jackhowerton@gmail.com>
2017-01-08 19:23:00 -0800

    7.2 complete

---
+++ js/data_structures.js
@@ -0,0 +1,44 @@
+var colors = ["red","pink","green","yellow"];
+
+var names = ["Jerry","Dave","Sprinkles","Rocket"];
+
+colors.push("purple");
+
+names.push("Charles");
+
+console.log(colors);
+console.log(names);
+
+
+var horses = {
+  
+}
+
+for (i = 0; i < names.length; i++) {
+  horses[names[i]] = colors[i]
+}
+
+console.log(horses);
+
+
+function Car(year,make,color,runs) {
+
+	this.year = year;
+	this.make = make;
+	this.color = color;
+	this.runs = runs
+
+	this.honk = function() {
+		console.log("HONK HONK!");
+	}
+
+	console.log("WE BUILT A CAR!")
+}
+
+var greased_lightning = new Car(2001,"Nissan Xterra","Yellow",false)
+
+var sally = new Car(1969,"Chevy Camaro","Orange",true)
+
+greased_lightning.honk()
+sally.honk()
+console.log(sally)
