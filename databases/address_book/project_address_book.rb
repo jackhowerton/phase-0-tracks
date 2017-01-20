1b132b6fbee6d56aea32bff54246be2ba94a720f
Jack Howerton <jackhowerton@gmail.com>
2017-01-19 21:05:17 -0800

    finished 8.5

--- databases/address_book/project_address_book.rb
+++ databases/address_book/project_address_book.rb
@@ -7,7 +7,6 @@ database = SQLite3::Database.new("address_book.db")
 # treats the rows of the database as a hash rather than array
 database.results_as_hash = true
 
-
 # method sets up "contacts" table in address_book.db ONLY if not already there.
 setup_table_cmd = <<-SQL
 	CREATE TABLE IF NOT EXISTS contacts(
@@ -22,6 +21,17 @@ SQL
 
 # calls above method to make sure "contacts" table exists in address_book.db
 database.execute(setup_table_cmd)
+
+# prints formatted version of input
+def print (input)
+	input.each do |contact|
+		puts "		#{contact['id']}. #{contact['firstname']} #{contact['lastname']}"
+	   	puts "		EMAIL: #{contact['email']}"
+	   	puts "		PHONE: #{contact['phone']}"
+	   	puts "		ADDRESS: #{contact['address']}"
+	   	puts ""
+	end
+end
 
 # adds a new contact in the database
 def create_new_contact(db)
@@ -49,37 +59,76 @@ def search (db)
 	search_term = nil;
 	results = nil;
 	
-	puts "What attributes would you like to search? (lastname, firstname, email, phone or address)"
+	puts "What attributes would you like to search? (id, lastname, firstname, email, phone or address)"
 	type = gets.chomp
 
 
 	puts "","Please enter the term you would like to search for."
 	search_term = gets.chomp
 
-	case type
-	when "lastname"
-		results = db.execute("SELECT * FROM contacts WHERE lastname=?",search_term)
-	when "firstname"
-		results = db.execute("SELECT * FROM contacts WHERE firstname=?",search_term)
-	when "email"
-		results = db.execute("SELECT * FROM contacts WHERE email=?",search_term)
-	when "phone"
-		results = db.execute("SELECT * FROM contacts WHERE phone=?",search_term)
-	when "address"
-		results = db.execute("SELECT * FROM contacts WHERE address=?",search_term)
-	else
-	  puts "User entered invalid command."
-	end
+	results = db.execute("SELECT * FROM contacts WHERE #{type}=?",search_term)
 
 	puts "","Contacts Found: #{results.length}",""
-   	results.each do |contact|
-   	puts "		#{contact['id']}. #{contact['firstname']} #{contact['lastname']}"
-   	puts "		EMAIL: #{contact['email']}"
-   	puts "		PHONE: #{contact['phone']}"
-   	puts "		ADDRESS: #{contact['address']}"
-   	puts ""
-   end
+   	print(results)
+
 end
 
-search(database)
+def remove_contact(db)
+	type = nil;
+	search_term = nil;
+	matches = nil;
+
+	puts "You may delete contact(s) by their id, lastname, firstname, email, phone or address. What type of term would you like to use to identify contacts for deletion?"
+	type = gets.chomp
+
+	puts "","Please enter the term."
+	search_term = gets.chomp
+
+	matches = db.execute("SELECT * FROM contacts WHERE #{type}=?",search_term)
+	puts "","#{matches.length} contacts have been deleted from the system.",""
+
+	db.execute("DELETE FROM contacts WHERE #{type}=?",search_term)
+end
+
+
+
+
+# Driver code and user prompt
+
+puts "Welcome to Address Book! Your current version is GREAT!",""
+
+user_exit = 0;
+until user_exit == 1;
+	
+	puts "","Please enter a command:",""
+	puts "		'VIEW' -- See printout of the entire address book."
+	puts "		'SEARCH' -- Search through the address book."
+	puts "		'ADD' -- Made a new friend? Fantastic! Add their info here!"
+	puts "		'DELETE' -- Added too much fake data? 'DELETE' it!"
+	puts "		'EXIT' -- Get out of here!"
+
+	while input = gets.chomp.downcase
+		case input
+		when 'view'
+			print(database.execute("SELECT * FROM contacts"))
+			break;
+		when 'search'
+			search(database)
+			break;
+		when 'add'
+			create_new_contact(database)
+			break;	
+		when 'delete'
+			remove_contact(database)
+			break;
+		when 'exit'
+			puts "Thanks for using the best address book ever!"
+			user_exit = 1;
+			break;
+		else
+			puts "","Invalid entry, please try again."
+		end
+	end
+end
+
 
